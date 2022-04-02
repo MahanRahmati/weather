c370b80b513eb4428e378d6cab7b934a8050629c
MahanRahmati <mahan.rahmati@protonmail.com>
2022-04-03 02:41:29 +0430

    Update HourlyWidget

--- lib/widgets/hourly_widget.dart
+++ lib/widgets/hourly_widget.dart
@@ -23,64 +23,79 @@ class HourlyWidget extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final time = ref.watch(changeTimeFormat);
     final temp = ref.watch(changeTempUnit);
-    return ArnaGroupedView(
+    return ArnaList(
       title: Strings.today,
-      children: [
-        SizedBox(
-          height: 140,
-          child: ScrollConfiguration(
-            behavior: ScrollConfiguration.of(context).copyWith(
-              scrollbars: false,
+      items: [
+        Padding(
+          padding: Styles.normal,
+          child: Container(
+            height: 140,
+            decoration: BoxDecoration(
+              borderRadius: Styles.borderRadius,
+              border: Border.all(
+                color: ArnaDynamicColor.resolve(
+                  ArnaColors.borderColor,
+                  context,
+                ),
+              ),
+              color: ArnaDynamicColor.resolve(ArnaColors.cardColor, context),
             ),
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: hourly.map((hourly) {
-                return Padding(
-                  padding: Styles.largeHorizontal,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: Styles.normal,
-                        child: Text(
-                          time.format == TimeFormat.t24
-                              ? DateFormat('H').format(hourly!.date!)
-                              : DateFormat('h a').format(hourly!.date!),
-                          style:
-                              ArnaTheme.of(context).textTheme.subtitleTextStyle,
-                        ),
-                      ),
-                      Padding(
-                        padding: Styles.normal,
-                        child: Container(
-                          height: 42,
-                          width: 42,
-                          clipBehavior: Clip.antiAlias,
-                          decoration: BoxDecoration(
-                            borderRadius: borderRadiusAll(42),
-                            color: circleBackgroundColor(context),
-                          ),
+            child: ScrollConfiguration(
+              behavior: ScrollConfiguration.of(context).copyWith(
+                scrollbars: false,
+              ),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: hourly.map((hourly) {
+                  return Padding(
+                    padding: Styles.largeHorizontal,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
                           padding: Styles.normal,
-                          child: SvgPicture.asset(
-                            'assets/images/${hourly.weatherIcon}.svg',
+                          child: Text(
+                            time.format == TimeFormat.t24
+                                ? DateFormat('H').format(hourly!.date!)
+                                : DateFormat('h a').format(hourly!.date!),
+                            style: ArnaTheme.of(context)
+                                .textTheme
+                                .subtitleTextStyle,
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: Styles.normal,
-                        child: Text(
-                          temp.tempUnit == Temp.celsius
-                              ? "${hourly.temperature!.celsius!.ceil()}°"
-                              : temp.tempUnit == Temp.fahrenheit
-                                  ? "${hourly.temperature!.fahrenheit!.ceil()}°"
-                                  : "${hourly.temperature!.kelvin!.ceil()}°",
-                          style: ArnaTheme.of(context).textTheme.titleTextStyle,
+                        Padding(
+                          padding: Styles.normal,
+                          child: Container(
+                            height: 42,
+                            width: 42,
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(
+                              borderRadius: borderRadiusAll(42),
+                              color: circleBackgroundColor(context),
+                            ),
+                            padding: Styles.normal,
+                            child: SvgPicture.asset(
+                              'assets/images/${hourly.weatherIcon}.svg',
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              }).toList(),
+                        Padding(
+                          padding: Styles.normal,
+                          child: Text(
+                            temp.tempUnit == Temp.celsius
+                                ? "${hourly.temperature!.celsius!.ceil()}°"
+                                : temp.tempUnit == Temp.fahrenheit
+                                    ? "${hourly.temperature!.fahrenheit!.ceil()}°"
+                                    : "${hourly.temperature!.kelvin!.ceil()}°",
+                            style:
+                                ArnaTheme.of(context).textTheme.titleTextStyle,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
             ),
           ),
         ),
